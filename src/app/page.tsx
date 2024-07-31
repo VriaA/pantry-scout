39dfb304d0e01d5597071ba2c0f4120ff7fd3023
Victoria <oyelolaabimbimbola23@gmail.com>
2024-07-31 06:34:35 +0100

    Implement adding items to Firestore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,20 @@
 import { AppContext } from "@/contexts/AppContext"
 import { TAppContext } from "@/types/app"
 import { Container, Typography } from "@mui/material"
+import { useState } from "react";
+import * as React from 'react';
+import Fab from '@mui/material/Fab';
+import { Add } from "@mui/icons-material";
 import { useContext, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import Loading from "./loading"
+import FormModal from "@/components/FormModal";
 
 export default function Home() {
   const { signedInUser } = useContext(AppContext) as TAppContext
   const router = useRouter()
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClickOpen = () =>  setIsOpen(true)
 
   useEffect(() => {
     if (signedInUser === null) {
@@ -23,6 +30,10 @@ export default function Home() {
   return (
     <Container>
       <Typography variant="body1">Pantry Scout</Typography>
+                <Fab color="primary" aria-label="add" onClick={handleClickOpen}>
+                    <Add />
+                </Fab>
+                <FormModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </Container>
   )
 }
---
+++ src/components/FormModal.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { addDoc, serverTimestamp, collection } from "firebase/firestore"
+import { db } from "@/firebase"
+import { AppContext } from '@/contexts/AppContext';
+import { TAppContext } from '@/types/app';
+
+type TFormModal = {
+    isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export default function FormModal({isOpen, setIsOpen}: TFormModal) {
+  const { setDialog, openDialog } = React.useContext(AppContext) as TAppContext
+
+  function addItem(formData: FormData) {
+    const newItem =  formData.get('item')
+    try {
+      addDoc(collection(db, "pantry"), {
+        name: newItem,
+        count: 1,
+        timestamp: serverTimestamp()
+      })
+    } catch(error: any) {
+        setDialog((prev)=> ({...prev, message: error.message}))
+        openDialog()
+      console.log(error)
+    } finally {
+        handleClose()
+    }
+  }
+
+  const handleClose = () => setIsOpen(false)
+
+  return (
+    <React.Fragment>
+      <Dialog
+        open={isOpen}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          action: addItem,
+        }}
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="item"
+            name="item"
+            label="Item Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+          <Button type="submit">Add item</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
