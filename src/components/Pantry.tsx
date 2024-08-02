2212b9a55bf490977253f04ff185d60b1e1d1e77
VriaA <123843734+VriaA@users.noreply.github.com>
2024-08-02 23:27:44 +0100

    adjust pantry style

--- src/components/Pantry.tsx
+++ src/components/Pantry.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Paper, Stack, Typography } from "@mui/material";
 import { Add, Remove } from "@mui/icons-material";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import { PantryContext, TPantryContext } from "@/contexts/PantryContext";
 import { useContext } from "react";
+import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported';
 
 export default function Pantry({ handleClickOpen }: { handleClickOpen: () => void }): JSX.Element {
     const { itemsToRender, loading, deleteItem, increaseQuantityByOne, decreaseQuantityByOne } = useContext(PantryContext) as TPantryContext
@@ -38,25 +39,36 @@ export default function Pantry({ handleClickOpen }: { handleClickOpen: () => voi
     }
 
     return (
-        <Stack>
+        <Stack spacing={2} direction="row" flexWrap={'wrap'} justifyContent="flex-start" width="100%">
             {itemsToRender.map((item) => (
-                <Box component="section" key={item.docId} display='flex' justifyContent='space-between'>
-                    <Typography variant="h2">{item.name}</Typography>
-                    <Box display='flex'>
-                        <Box display='flex' alignItems='center'>
-                            <Button onClick={() => increaseQuantityByOne(item.docId, item.quantity)}>
-                                <Add />
-                            </Button>
-                            <Typography variant="body2">{item.quantity}</Typography>
-                            <Button onClick={() => decreaseQuantityByOne(item.docId, item.quantity)}>
-                                <Remove />
+                <Paper component="section" key={item.docId} sx={{ width: '300px' }}>
+                    {item.imageSrc ?
+                        <img width={"100%"} height={200} style={{ objectFit: "cover", objectPosition: "center" }} src={item.imageSrc} alt={item.name} />
+                        :
+                        <Box position="relative" display="flex" width="100%" height={200} sx={{ bgcolor: '#99999920' }}>
+                            <ImageNotSupportedIcon fontSize='large' sx={{
+                                position: 'absolute', display: 'block', zIndex: 0, inset: 0, margin: 'auto', fill: '#b3b3b350',
+                            }} />
+                        </Box>
+                    }
+                    <Box display='flex' justifyContent='space-between' flex="none">
+                        <Typography variant="h2">{item.name}</Typography>
+                        <Box display='flex'>
+                            <Box display='flex' alignItems='center'>
+                                <Button onClick={() => increaseQuantityByOne(item.docId, item.quantity)}>
+                                    <Add />
+                                </Button>
+                                <Typography variant="body2">{item.quantity}</Typography>
+                                <Button onClick={() => decreaseQuantityByOne(item.docId, item.quantity)}>
+                                    <Remove />
+                                </Button>
+                            </Box>
+                            <Button onClick={() => deleteItem(item.docId)}>
+                                <DeleteOutlineOutlinedIcon />
                             </Button>
                         </Box>
-                        <Button onClick={() => deleteItem(item.docId)}>
-                            <DeleteOutlineOutlinedIcon />
-                        </Button>
                     </Box>
-                </Box>
+                </Paper>
             ))}
         </Stack>
     );
--- src/contexts/PantryContext.tsx
+++ src/contexts/PantryContext.tsx
@@ -64,7 +64,7 @@ export default function PantryContextProvider({ children }: { children: ReactNod
             quantity: 1,
             userId: signedInUser?.uid,
             timestamp: serverTimestamp(),
-            imageSrc: image
+            imageSrc: image ? image : null
         })
     }
 
